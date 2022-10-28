952ab9c1234eed0c5a54eeb7c9f0184e72aa7889
Valeria <valeriaalarcon@Valerias-MacBook-Pro.local>
2022-10-28 13:42:40 -0400

    Fixed diagnositcs.js and index.js

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -5,11 +5,19 @@ const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 // GET Route for retrieving diagnostic information
 diagnostics.get('/', (req, res) => {
   // TODO: Logic for sending all the content of db/diagnostics.json
+  res.json(diagnostics);
 });
 
 // POST Route for a error logging
 diagnostics.post('/', (req, res) => {
   // TODO: Logic for appending data to the db/diagnostics.json file
+  const newDiagnostic = req.body;
+  // add updated object ot notes array
+  diagnostics.push(newDiagnostic);
+
+  // updated db.json file
+  FileSystem.writeFile(`./db/db.json`), JSON.stringify(diagnostics, null, 2), err =>
+  err ? console.error(err) : console.info(`New diagnositc has been written to json file`)
 });
 
 module.exports = diagnostics;
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,16 @@ const express = require('express');
 // Import our modular routers for /tips and /feedback
 const tipsRouter = require('./tips');
 const feedbackRouter = require('./feedback');
+
 // TODO: import your diagnostics route
+const diagnosticsRouter = require('./disagnostics');
 
 const app = express();
 
 app.use('/tips', tipsRouter);
 app.use('/feedback', feedbackRouter);
+
 // TODO: Initialize diagnostics route
+app.use('/diagnostics', diagnosticsRouter);
 
 module.exports = app;
